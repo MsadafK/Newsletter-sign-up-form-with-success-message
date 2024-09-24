d93365a3912c9a29b0157ca070cec8b61d440dcd
Mohd Sadaf <mssadaf0@gmail.com>
2024-09-25 03:27:07 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,59 @@
+// Get form elements
+const newsletter = document.querySelector(".newsletter");
+const form = document.querySelector(".newsletter__form");
+const email = document.getElementById("email");
+const successContainer = document.querySelector(".success-container");
+const dismissButton = document.querySelector(".success-button");
+
+// Regular expressions for validation
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+let flag = false;
+
+// Form submission event listener
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  let isValid = true;
+
+  // Validate email
+  if (!emailRegex.test(email.value.trim())) {
+    showOrRemoveError(email, "Please enter a valid email address");
+    isValid = false;
+  } else {
+    showOrRemoveError(email);
+  }
+
+  // If validation pass, submit the form
+  if (isValid) {
+    form.reset();
+    newsletter.classList.add("hide");
+    successContainer.classList.remove("hide");
+    flag = true;
+  }
+});
+
+// Dismiss message event listener
+dismissButton.addEventListener("click", function () {
+  if (flag) {
+    newsletter.classList.remove("hide");
+    successContainer.classList.add("hide");
+    flag = false;
+  }
+});
+
+// Function to show error messages
+function showOrRemoveError(element, message) {
+  const parent =
+    element.closest(".newsletter__form-group") || element.parentElement;
+  const errorSpan = parent.querySelector("span[id$='-error]");
+
+  if (message) {
+    errorSpan.textContent = message;
+    errorSpan.classList.add("error-message");
+    element.setAttribute("aria-invalid", "true");
+  } else {
+    errorSpan.textContent = "";
+    errorSpan.classList.remove("error-message");
+    element.setAttribute("aria-invalid", "false");
+  }
+}
